04d551121b3b5cb04df9c41982f6e38f60d82a01
PolRubio <aporcrb23@gmail.com>
2022-12-04 18:48:23 +0100

    basic structure of the class

--- src/Producte/Producte.java
+++ src/Producte/Producte.java
@@ -1,5 +1,33 @@
 package Producte;
 
 public class Producte {
+    private String[] dades;
+    private int identificador;
+    private Producte[] reaccions;
+
+    public Producte(String[] dades, int identificador, Producte[] reaccions) {
+        this.dades = dades;
+        this.identificador = identificador;
+        this.reaccions = reaccions;
+    }
+
+    public String[] getDades() {
+        return dades;
+    }
+    public int getIdentificador() {
+        return identificador;
+    }
+    public Producte getReaccio(int i) {
+        return reaccions[i];
+    }
     
+    public void setDades(String[] dades) {
+        this.dades = dades;
+    }
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+    public void setReaccio(int i, Producte reaccio) {
+        this.reaccions[i] = reaccio;
+    }
 }
